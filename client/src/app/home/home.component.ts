66a73c6e4936ce3a81dd2e211d70403ae3e6cc7a
Subhikshaa23 <210701264@rajalakshmi.edu.in>
2024-02-23 23:34:31 +0530

    Toggle login and register form

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
     
     constructor(){}
+
+    showLoginForm: boolean = true;
+    toggleForm() {
+      this.showLoginForm = !this.showLoginForm;
+    }
+
     onSubmit(formData : NgForm){
       console.log(formData.controls['email'].value);
       console.log(formData.controls['password'].value);
