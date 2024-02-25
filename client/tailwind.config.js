4a0486166a5168303b746d4ddfe336f35f9a7ddf
Syed-Javith <syedjavith14@gmail.com>
2024-02-25 19:09:54 +0530

    tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,8 @@
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.html', './src/**/*.ts'
-  ],
+  content: ['./src/**/*.{html,ts}', './FoodManagement/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+ }
